c5b3f71347ba64b2c15f9149c46fc4a76d50d913
leonid <leonidgadetsky@yandex.ru>
2022-08-09 00:08:02 +0300

    some ref

--- src/main/java/com/lgadetsky/orderservice/OrderServiceApplication.java
+++ src/main/java/com/lgadetsky/orderservice/OrderServiceApplication.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-/* TODO
- * 1) Make right version of repository pattern
- * 2) add dto for order class and used it 
- * 3) repair update function*/
-
 @SpringBootApplication
 @ServletComponentScan
 public class OrderServiceApplication {
