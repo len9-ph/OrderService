56930d5cf76043d7166a0b5d85bec88cad626ff4
leonid <leonidgadetsky@yandex.ru>
2022-08-10 21:36:18 +0300

    Mapper scratch

--- src/main/java/com/lgadetsky/orderservice/repository/mapper/Mapper.java
+++ src/main/java/com/lgadetsky/orderservice/repository/mapper/Mapper.java
@@ -3,10 +3,30 @@
  */
 package com.lgadetsky.orderservice.repository.mapper;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.lgadetsky.orderservice.model.Order;
+import com.lgadetsky.orderservice.model.OrderItem;
+import com.lgadetsky.orderservice.model.dto.OrderDTO;
+
 /**
- * @author leoni
+ * @author Leonid Gadetsky
  *
  */
+@Component
 public class Mapper {
-
+	public OrderDTO toDTO(Order order, List<OrderItem> items) {
+		return new OrderDTO();
+	}
+	
+	public Order toOrder(OrderDTO dto) {
+		return new Order();
+	}
+	
+	public List<OrderItem> toItems(OrderDTO dto){
+		return new LinkedList<OrderItem>();
+	}
 }
