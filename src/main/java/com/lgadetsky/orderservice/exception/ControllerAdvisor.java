326ed054b21e7ded568daa15b701e4b45967c0d7
leonid <leonidgadetsky@yandex.ru>
2022-08-29 18:36:00 +0300

    control advisor ref

--- src/main/java/com/lgadetsky/orderservice/exception/ControllerAdvisor.java
+++ src/main/java/com/lgadetsky/orderservice/exception/ControllerAdvisor.java
@@ -2,14 +2,10 @@ package com.lgadetsky.orderservice.exception;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -17,34 +13,18 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler{
+	private static final String TIMESTAMP = "timestamp";
+	private static final String MESSAGE = "timestamp";
+	private static final String ORDER_NOT_FOUND = "Order not found";
 	
 	@ExceptionHandler(OrderNotFoundException.class)
 	public ResponseEntity<Object> handleOrderNotFoundException(
 			OrderNotFoundException ex, WebRequest request){
 		
 		Map<String, Object> body = new LinkedHashMap<>();
-		body.put("timestamp",LocalDateTime.now());
-		body.put("message", "Order not found");
+		body.put(TIMESTAMP, LocalDateTime.now());
+		body.put(MESSAGE, ORDER_NOT_FOUND);
 		return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-	}
-	
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(
-			MethodArgumentNotValidException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		
-		Map<String, Object> body = new LinkedHashMap<>();
-		body.put("timestamp",LocalDateTime.now());
-		body.put("message", "Order not found");
-		
-		List<String> errors = ex.getBindingResult()
-				.getFieldErrors()
-				.stream()
-				.map(x -> x.getDefaultMessage())
-				.collect(Collectors.toList());
-		
-		body.put("errors", errors);
-		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
 	}
 
 }
